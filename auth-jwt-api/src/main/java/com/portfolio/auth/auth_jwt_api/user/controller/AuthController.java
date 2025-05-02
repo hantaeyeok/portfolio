8b358e224cfc3e6030b566a573791289d2096d18
hantaeyeok <160584121+hantaeyeok@users.noreply.github.com>
2025-05-02 20:14:40 +0900

    commit

--- auth-jwt-api/src/main/java/com/portfolio/auth/auth_jwt_api/user/controller/AuthController.java
+++ auth-jwt-api/src/main/java/com/portfolio/auth/auth_jwt_api/user/controller/AuthController.java
@@ -1,5 +1,14 @@
 package com.portfolio.auth.auth_jwt_api.user.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/login")
+@RequiredArgsConstructor
 public class AuthController {
     
 }
